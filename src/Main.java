6f0af05e0aa3f9388c828ab4aa2af9d886080c9d
zakura1x <zeusasico.tenerife@unc.edu.ph>
2022-11-18 19:45:49 +0800

    Add Comments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 /*
- * @Title: UNION AND INTERSECTION OF SETS
+ * @Title: BINGO TICKET GENERATOR
  * @Description: Develop a program that will generate a BINGO ticket 
  *               which is consists of four unique bingo cards. The program will display the 
  *               bingo cards with this given template format
